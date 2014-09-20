fbc29bbd2a4216a635939732ec8a586c5fe0c97c
Alexander Gamaunov <unlike777@gmail.com>
2014-09-21 00:35:01 +0700

    Скоректировал журанл под новый формат логов

--- modules/jornal/__system.php
+++ modules/jornal/__system.php
@@ -11,6 +11,7 @@ class __system {
         ui::addLeftButton('Журнал БД', 'db_view');
 
         function removeQuotes($val, $obj){
+			$val = trim($val);
         	return substr($val, 1, strlen($val) - 2);
         }
 
@@ -36,7 +37,7 @@ class __system {
 	    	while(list($key, $val) = each($file)){
                 $tmp = explode(Chr(9), $val);
                 if (!empty($tmp[1])) {
-                    $tmp[1] = strtotime(removeQuotes($tmp[1], $tmp[1]));    
+                    $tmp[0] = strtotime(removeQuotes($tmp[0], $tmp[0]));    
                     $tmp_mas[] = $tmp;
                 }
             }
@@ -54,14 +55,19 @@ class __system {
             
 	    } else $count = 0;
 
-
 	    $table = new uiTable($mas, $count);
-	    $table->emptyText('В системном журнале нет записей!');
-        $table->addColumn('2', 'Важность', 0, false, false, 'removeQuotes');
-        $table->addColumn('3', 'Пользователь', 0, false, false, 'removeQuotes');
-        $table->addColumn('4', 'Действие', 400);
-        $table->addColumn('1', 'Дата / Время', 0, false, false, 'rqDateTime');
-        $table->addColumn('0', 'IP');
+		$table->emptyText('В системном журнале нет записей!');
+		$table->addColumn('1', 'Важность', 0, false, false, 'removeQuotes');
+		$table->addColumn('3', 'Пользователь', 0, false, false, 'removeQuotes');
+		$table->addColumn('4', 'Действие', 400);
+		$table->addColumn('2', 'IP', 0, false, false, 'removeQuotes');
+		$table->addColumn('0', 'Дата / Время', 0, false, false, 'rqDateTime');
+		
+		
+		
+		
+		
+		
 
         return $table->getHTML();
  	}
